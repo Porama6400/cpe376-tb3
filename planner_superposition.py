1b9471f7f5148d6eae2e5274806b6c5d6e62f2d8
Porama6400 <porama@porama.dev>
2023-11-16 14:39:17 +0700

    4.1

--- planner_superposition.py
+++ planner_superposition.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from planner import SIDE_LEFT, SIDE_RIGHT, SIDE_BOTTOM, SIDE_TOP, SIDE_NULL, PlannerWorld, planner_delta_angle
 from planner_position import PlannerPosition
 
@@ -7,12 +9,10 @@ from planner_position import PlannerPosition
 class PlannerSuperPosition(object):
     def __init__(self, world: PlannerWorld):
         self.possible_positions: list = []
-        self.certain_position = None
         self.world = world
 
-    def add(self, x: int, y: int, direction: int):
-        wrapped = PlannerPosition(x, y, direction)
-        self.possible_positions.append(wrapped)
+    def add(self, pos: PlannerPosition):
+        self.possible_positions.append(pos)
 
     def step(self):
         for position in self.possible_positions:
@@ -40,33 +40,73 @@ class PlannerSuperPosition(object):
 
         self.possible_positions = temp_list
 
+    def populate_all(self):
+        for i in range(self.world.width):
+            for j in range(self.world.height):
+                self.add(PlannerPosition(i, j, SIDE_RIGHT))
+                self.add(PlannerPosition(i, j, SIDE_TOP))
+                self.add(PlannerPosition(i, j, SIDE_LEFT))
+                self.add(PlannerPosition(i, j, SIDE_BOTTOM))
+
     def print(self):
-        print("All possible positions:")
+        print("All possible positions:", self.count_possible_positions())
         for position in self.possible_positions:
             planner_position: PlannerPosition = position
             print(planner_position.x, planner_position.y, "@", planner_position.direction, "( start positon ",
                   planner_position.start_x, planner_position.start_y, "@", planner_position.start_direction, ")")
+
+    def count_possible_positions(self) -> int:
+        return len(self.possible_positions)
+
+    def is_collapsed(self):
+        return self.count_possible_positions() <= 1
+
+    def get_certain_position(self) -> Any | None:
+        count = self.count_possible_positions()
+        if count == 0:
+            raise Exception("unable to solve for position: no possible valid position")
+        elif count == 1:
+            return self.possible_positions[0]
+        else:
+            return None
+
 
 
 # flak noexport
 
 world = PlannerWorld(5, 3)
 world.set_wall(0, 0, SIDE_RIGHT, False)
+world.set_wall(0, 0, SIDE_BOTTOM, False)
 world.set_wall(1, 0, SIDE_RIGHT, False)
 world.set_wall(2, 0, SIDE_RIGHT, False)
 world.set_wall(2, 0, SIDE_BOTTOM, False)
 world.set_wall(3, 0, SIDE_RIGHT, False)
+world.set_wall(3, 0, SIDE_BOTTOM, False)
 world.set_wall(4, 0, SIDE_BOTTOM, False)
 
 world.set_wall(0, 1, SIDE_RIGHT, False)
 world.set_wall(0, 1, SIDE_BOTTOM, False)
+world.set_wall(1, 1, SIDE_RIGHT, False)
+world.set_wall(1, 1, SIDE_BOTTOM, False)
 world.set_wall(2, 1, SIDE_BOTTOM, False)
+world.set_wall(4, 1, SIDE_BOTTOM, False)
 
 world.set_wall(0, 2, SIDE_RIGHT, False)
-world.set_wall(1, 2, SIDE_RIGHT, False)
 world.set_wall(2, 2, SIDE_RIGHT, False)
 world.set_wall(3, 2, SIDE_RIGHT, False)
 
-pos = PlannerPosition(0, 0, SIDE_TOP)
-generated = pos.generate(world, 1, 1)
-print(generated)
+psp = PlannerSuperPosition(world)
+psp.populate_all()
+psp.step()
+psp.validate(True, False, True)
+psp.step()
+psp.validate(True, False, False)
+psp.step()
+psp.validate(True, False, False)
+psp.step()
+psp.validate(True, True, False)
+psp.print()
+print(psp.get_certain_position())
+
+# generated = pos.generate(world, 3, 1)
+# print(generated)
---
+++ turtlebot3_controller_41.py
@@ -0,0 +1,202 @@
+from dis import dis
+from socket import TIPC_SUBSCR_TIMEOUT
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+
+from sensor_msgs.msg import LaserScan, BatteryState
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+# from std_msgs.msg import String
+
+import math
+
+# flak omit
+from angle_util import angle_correction_min
+from array_average import ArrayAverage
+from pid_controller import PidController
+from mayson_controller import *
+from vector import Vector
+from planner import *
+from planner_superposition import *
+
+
+# flak unomit
+
+class Turtlebot3Controller(Node):
+
+    def __init__(self):
+        super().__init__('turtlebot3_controller')  # node name
+        self.cmdVelPublisher = self.create_publisher(Twist, 'cmd_vel', 1)
+        self.scanSubscriber = self.create_subscription(LaserScan, 'scan', self.scanCallback,
+                                                       qos_profile=qos_profile_sensor_data)
+        self.batteryStateSubscriber = self.create_subscription(BatteryState, 'battery_state', self.batteryStateCallback,
+                                                               1)
+        self.odomSubscriber = self.create_subscription(Odometry, 'odom', self.odomCallback, 1)
+
+        self.valueLaserRanges = [0.0] * 360
+        self.valueBatteryState = None
+        self.valuePosition = None
+        self.valueOrientation = None
+        self.valueRotation = 0
+
+        self.state = 0
+        # Use this timer for the job that should be looping until interrupted
+        self.timer = self.create_timer(0.1, self.timerCallback)
+
+        self.pid_linear = PidController()
+        self.pid_linear.output_cap = 0.15
+        self.pid_linear.proportional_gain = 0.8
+        self.pid_linear.integral_gain = 0.5
+        self.pid_linear.integral_cap = 0.03
+        self.pid_linear.integral_time = 8
+
+        self.pid_angular = PidController()
+        self.pid_angular.proportional_gain = 1.5
+        self.pid_angular.integral_gain = 0.2
+        self.pid_angular.integral_time = 8
+        self.pid_linear.integral_cap = 0.01
+        self.pid_angular.output_cap = 2
+
+        self.controller = MaysonController()
+        self.tick_counter = 0
+        self.near_threshold = 0.2
+
+        self.collapsed = False
+        self.world = PlannerWorld(5, 3)
+        world.set_wall(0, 0, SIDE_RIGHT, False)
+        world.set_wall(0, 0, SIDE_BOTTOM, False)
+        world.set_wall(1, 0, SIDE_RIGHT, False)
+        world.set_wall(2, 0, SIDE_RIGHT, False)
+        world.set_wall(2, 0, SIDE_BOTTOM, False)
+        world.set_wall(3, 0, SIDE_RIGHT, False)
+        world.set_wall(3, 0, SIDE_BOTTOM, False)
+        world.set_wall(4, 0, SIDE_BOTTOM, False)
+
+        world.set_wall(0, 1, SIDE_RIGHT, False)
+        world.set_wall(0, 1, SIDE_BOTTOM, False)
+        world.set_wall(1, 1, SIDE_RIGHT, False)
+        world.set_wall(1, 1, SIDE_BOTTOM, False)
+        world.set_wall(2, 1, SIDE_BOTTOM, False)
+        world.set_wall(4, 1, SIDE_BOTTOM, False)
+
+        world.set_wall(0, 2, SIDE_RIGHT, False)
+        world.set_wall(2, 2, SIDE_RIGHT, False)
+        world.set_wall(3, 2, SIDE_RIGHT, False)
+
+        self.planner = PlannerSuperPosition(world)
+        self.planner.populate_all()
+
+    def publishVelocityCommand(self, linearVelocity, angularVelocity):
+        msg = Twist()
+        msg.linear.x = linearVelocity
+        msg.angular.z = angularVelocity
+        self.cmdVelPublisher.publish(msg)
+        # self.get_logger().info('Publishing cmd_vel: "%s", "%s"' % linearVelocity, angularVelocity)
+
+    def scanCallback(self, msg):
+        self.valueLaserRanges = list(msg.ranges)
+
+    def batteryStateCallback(self, msg):
+        self.valueBatteryState = msg
+
+    def odomCallback(self, msg):
+        self.valuePosition = msg.pose.pose.position
+        self.valueOrientation = msg.pose.pose.orientation
+
+        siny_cosp = 2 * (
+                self.valueOrientation.w * self.valueOrientation.z + self.valueOrientation.x * self.valueOrientation.y
+        )
+        cosy_cosp = 1 - 2 * (
+                self.valueOrientation.y * self.valueOrientation.y + self.valueOrientation.z * self.valueOrientation.z
+        )
+        self.valueRotation = math.atan2(siny_cosp, cosy_cosp)
+
+    def timerCallback(self):
+        self.tick_counter += 1
+        ros_pos = Vector(self.valuePosition.x, self.valuePosition.y)
+        distance_left = angle_distance(self.valueLaserRanges, 90 - 5, 90 + 5)
+        distance_top = angle_distance(self.valueLaserRanges, 0 - 5, 0 + 5)
+        distance_right = angle_distance(self.valueLaserRanges, 270 - 5, 270 + 5)
+        near_left = distance_left < self.near_threshold
+        near_top = distance_top < self.near_threshold
+        near_right = distance_right < self.near_threshold
+
+        if not self.collapsed:
+            try:
+                self.controller.tick(ros_pos, self.valueRotation, self.valueLaserRanges)
+            except Exception as ex:
+                print(ex)
+                self.planner.validate(near_left, near_top, near_right)
+                if not near_top:
+                    self.controller.enqueue(MC_FWD)
+                    self.planner.step()
+                elif not near_left:
+                    self.controller.enqueue(MC_TURN_LEFT)
+                    self.planner.turn(1)
+                elif not near_right:
+                    self.controller.enqueue(MC_TURN_RIGHT)
+                    self.planner.turn(-1)
+                else:
+                    self.controller.enqueue(MC_TURN_U)
+                    self.planner.turn(2)
+
+                if self.planner.is_collapsed():
+                    self.collapsed = True
+
+                    position: PlannerPosition = self.planner.get_certain_position()
+                    print("certain position known:", position.x, position.y)
+                    plan = position.generate(self.world, 3, 1)
+                    for instruction in plan:
+                        self.controller.enqueue(instruction)
+
+        else:
+            self.controller.tick(ros_pos, self.valueRotation, self.valueLaserRanges)
+
+        speed = self.pid_linear.tick(self.controller.delta_distance)
+        angle = self.pid_angular.tick(self.controller.delta_angle)
+
+        print("==========")
+        print(self.controller.actual_position, self.controller.actual_heading)
+        print(speed, angle)
+        self.publishVelocityCommand(float(speed), float(angle))
+
+
+def robotStop():
+    node = rclpy.create_node('tb3Stop')
+    publisher = node.create_publisher(Twist, 'cmd_vel', 1)
+    msg = Twist()
+    msg.linear.x = 0.0
+    msg.angular.z = 0.0
+    publisher.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    tb3ControllerNode = Turtlebot3Controller()
+    print('tb3ControllerNode created')
+    try:
+        rclpy.spin(tb3ControllerNode)
+    except KeyboardInterrupt as ki:
+        print("KI")
+    except Exception as ex:
+        print(ex)
+    print('Done')
+
+    tb3ControllerNode.publishVelocityCommand(0.0, 0.0)
+    tb3ControllerNode.destroy_node()
+    robotStop()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
+# flak emplacement vector
+# flak emplacement angle_util
+# flak emplacement pid_controller
+# flak emplacement mayson_controller
+# flak emplacement array_average
+# flak emplacement stab_checker
+# flak emplacement planner
+# flak emplacement planner_position
